7c3a2285b88e3bb29045e05cd96af5b586a12c6a
Ondřej Němec <ondrej.nemec@autmes.cz>
2019-04-11 12:07:48 +0200

    added mssql implementation of dbadapter

---
+++ src/DbAdapters/SqlServer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PetrKnap\Php\MigrationTool\DbAdapters;
+
+/**
+ * SqlServerAdapter
+ *
+ * @author Ondřej Němec <ondrej.nemec@autmes.cz>
+ */
+class SqlServerAdapter implements DbAdapter {
+
+    public function createTableIfNotExists($tableName) {
+        return 'IF NOT EXISTS (SELECT * FROM sysobjects WHERE name=\'' . $tableName . '\' and xtype=\'U\')'.
+                       ' CREATE TABLE  . $tableName .  ('.
+                            'id VARCHAR(16) NOT NULL PRIMARY KEY,' .
+                            'applied DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP' .
+                        ')'.
+                    ' GO';
+    }
+
+    public function getDbType() {
+        return 'sqlsvr';
+    }
+
+    public function insertIntoTable($tableName) {
+        return 'INSERT INTO ' . $tableName . ' (id) VALUES (:id)';
+    }
+
+    public function selectNullFromId($tableName) {
+         return 'SELECT TOP 1 null FROM ' . $tableName;
+    }
+
+    public function selectOneNull($tableName) {
+        return 'SELECT null FROM ' . $tableName . ' WHERE id = :id';
+    }
+
+}
